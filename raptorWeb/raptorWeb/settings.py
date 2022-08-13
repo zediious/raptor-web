1d24083e70225db682e38057d66c995d6f179115
Jack <zediious@aol.com>
2022-08-13 06:44:28 -0400

    static files

--- raptorWeb/raptorWeb/settings.py
+++ raptorWeb/raptorWeb/settings.py
@@ -10,12 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-import os
+from os.path import join
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
+TEMPLATE_DIR = join(BASE_DIR, "templates")
+STATIC_DIR = join(BASE_DIR, "static")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -120,6 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# THIS MUST BE NAMED `STATICFILES_DIRS`
+STATICFILES_DIRS = [
+
+    STATIC_DIR,
+
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
