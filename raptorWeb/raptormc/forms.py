783cef12b5f82ec2d679526ee1805196d3ef5a5a
Jack <zediious@aol.com>
2022-08-21 01:52:56 -0400

    Django built-in validators

--- raptorWeb/raptormc/forms.py
+++ raptorWeb/raptormc/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core import validators
 import logging
 
 LOGGER = logging.Logger("form_validator_logger")
@@ -9,29 +10,29 @@ class AdminApp(forms.Form):
     email = forms.EmailField()
     whether_developer = forms.BooleanField(label="Do you have a background in Software Development, particularly back-end?")
     why_join = forms.CharField(widget=forms.Textarea)
-    botTrapA = forms.CharField(required=False, widget=forms.HiddenInput)
+    bot_trap = forms.CharField(required=False, widget=forms.HiddenInput, validators=[validators.MaxLengthValidator(0)])
 
-    def clean_botTrapA(self):
+    # def clean_botTrapA(self):
 
-        botTrapA = self.cleaned_data['botTrapA']
+    #     botTrapA = self.cleaned_data['botTrapA']
 
-        if len(botTrapA) > 0:
+    #     if len(botTrapA) > 0:
 
-            LOGGER.error("[WARN] Hidden input field was filled out in AdminApp, likely a BOT.")
-            raise forms.ValidationError("")
+    #         LOGGER.error("[WARN] Hidden input field was filled out in AdminApp, likely a BOT.")
+    #         raise forms.ValidationError("")
 
 class ModApp(forms.Form):
 
     name = forms.CharField()
     email = forms.EmailField()
     why_join = forms.CharField(widget=forms.Textarea)
-    botTrapM = forms.CharField(required=False, widget=forms.HiddenInput)
+    bot_trap = forms.CharField(required=False, widget=forms.HiddenInput, validators=[validators.MaxLengthValidator(0)])
 
-    def clean_botTrapM(self):
+    # def clean_botTrapM(self):
 
-        botTrapM = self.cleaned_data['botTrapM']
+    #     botTrapM = self.cleaned_data['botTrapM']
 
-        if len(botTrapM) > 0:
+    #     if len(botTrapM) > 0:
 
-            LOGGER.error("[WARN] Hidden input field was filled out in ModApp, likely a BOT.")
-            raise forms.ValidationError("")
+    #         LOGGER.error("[WARN] Hidden input field was filled out in ModApp, likely a BOT.")
+    #         raise forms.ValidationError("")
