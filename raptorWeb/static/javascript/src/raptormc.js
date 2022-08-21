32925ba315d696a292b75d283ad2001e45009b4e
Jack <zediious@aol.com>
2022-08-21 01:03:46 -0400

    Scroll-linked background position + webp
    - Added JavaScript to lnik the background position to the scrolling action, such that it will scroll "slower" than the rest of the page. Applied to all pages
    
    - Convered all images to `.webp` format.
    - Remade indicator images to a higher resolution.

--- raptorWeb/static/javascript/src/raptormc.js
+++ raptorWeb/static/javascript/src/raptormc.js
@@ -1,1 +1,10 @@
 console.log("raptormc.js loaded")
+
+document.getElementById("body").onscroll = function myFunction() {  
+    var scrolltotop = document.scrollingElement.scrollTop;
+    var target = document.getElementById("headerBox");
+    var xvalue = "center";
+    var factor = 0.6;
+    var yvalue = scrolltotop * factor;
+    target.style.backgroundPosition = xvalue + " " + yvalue + "px";
+  }
