3d21ff0a5ca4f79764c44847cfd65a9e5c29a0f8
Jack <zediious@aol.com>
2022-09-05 23:26:04 -0400

    Implement clipboard.js for copying addresses.
    - Added jquery and clipboard.js as javascript dependencies
    - Created ClipboardJS instances for each server address.
    - Formatted server addresses in modals into buttons, Clicking these buttons will copy the respective server's address to the clipboard.
    
    - Modified hexxit's state to read from nomi's info. The proxy causes these queries to be pretty surface level, and not specific to each server. It is likely I will just not run 1.12.2 servers through the proxy anymore.

--- raptorWeb/static/javascript/src/raptormc.js
+++ raptorWeb/static/javascript/src/raptormc.js
@@ -1,5 +1,31 @@
 console.log("raptormc.js loaded")
 
+$( document ).ready(function() {
+  new ClipboardJS('.copyNomiAddress', {
+    container: document.getElementById('nomiDesc')
+});
+
+  new ClipboardJS('.copye6eAddress', {
+    container: document.getElementById('e6eDesc')
+});
+
+  new ClipboardJS('.copyct2Address', {
+    container: document.getElementById('ct2Desc')
+});
+
+  new ClipboardJS('.copyftbuAddress', {
+    container: document.getElementById('ftbuDesc')
+});
+
+  new ClipboardJS('.copyobAddress', {
+    container: document.getElementById('obDesc')
+});
+
+  new ClipboardJS('.copyhexxitAddress', {
+    container: document.getElementById('hexxitDesc')
+});
+});
+
 document.getElementById("body").onscroll = function myFunction() {  
     var scrolltotop = document.scrollingElement.scrollTop;
     var target = document.getElementById("headerBox");
