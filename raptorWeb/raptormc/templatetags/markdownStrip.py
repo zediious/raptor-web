19438283d33c871ec40069ec00a601582da509f3
zediious <zediious@aol.com>
2022-11-26 20:00:51 -0500

    new_tab_black source from settings domain

--- raptorWeb/raptormc/templatetags/markdownStrip.py
+++ raptorWeb/raptormc/templatetags/markdownStrip.py
@@ -3,6 +3,8 @@ from re import sub, search
 from django import template
 from django.utils.html import urlize
 
+from raptorWeb import settings
+
 register = template.Library()
 
 @register.filter
@@ -38,7 +40,7 @@ def https_to_discord(value):
     if anchor:
         blank_anchor = anchor.group(0).replace('<a', '<a target="_blank"')
         anchor_end = search(r'</a>', initial)
-        anchor_end_icon = anchor_end.group(0).replace('</a>', ' <img class="new_tab_icon" src="https://shadowraptor.net/static/image/new_tab_black.svg"></a>')
+        anchor_end_icon = anchor_end.group(0).replace('</a>', f' <img class="new_tab_icon" src="https://{settings.DOMAIN_NAME}/static/image/new_tab_black.svg"></a>')
         return initial.replace(anchor.group(0), blank_anchor).replace(anchor_end.group(0), anchor_end_icon)
     
     return initial
