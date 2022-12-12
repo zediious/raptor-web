b142c9b5087f805719b99a08b5f0ea251c063b4d
zediious <zediious@aol.com>
2022-12-12 17:45:40 -0500

    Fix forgotten Network Rules InformativeText

--- raptorWeb/raptormc/views.py
+++ raptorWeb/raptormc/views.py
@@ -60,7 +60,9 @@ class ShadowRaptor():
             def get_context_data(self, **kwargs):
                 context = super().get_context_data(**kwargs)
                 context.update(player_poller.currentPlayers_DB)
-                return viewContext.update_context(context = context, informative_text_names = ["Rules Information"])
+                return viewContext.update_context(context = context, informative_text_names = [
+                    "Rules Information",
+                    "Network Rules"])
 
         class BannedItems(TemplateView):
             """
