d08d617085d1fe3ec7c594137d8ea530c9aa3799
zediious <zediious@aol.com>
2022-12-17 02:44:27 -0500

    Use django-jazzmin for admin
    Nice app that makes the Django Admin a bit nicer. I plan to customize this quite a bit

--- raptorWeb/staffapps/admin.py
+++ raptorWeb/staffapps/admin.py
@@ -1,20 +1,6 @@
 from django.contrib import admin
-from django.forms import TextInput, Textarea
-from django.db import models
 
 from staffapps.models import ModeratorApplication, AdminApplication
 
-class AdminApplicationAdmin(admin.ModelAdmin):
-    formfield_overrides = {
-        models.CharField: {'widget': TextInput(attrs={'size':'35'})},
-        models.TextField: {'widget': Textarea(attrs={'rows':6, 'cols':60})},
-    }
-
-class ModApplicationAdmin(admin.ModelAdmin):
-    formfield_overrides = {
-        models.CharField: {'widget': TextInput(attrs={'size':'35'})},
-        models.TextField: {'widget': Textarea(attrs={'rows':6, 'cols':60})},
-    }
-
-admin.site.register(AdminApplication, AdminApplicationAdmin)
-admin.site.register(ModeratorApplication, ModApplicationAdmin)
+admin.site.register(AdminApplication)
+admin.site.register(ModeratorApplication)
