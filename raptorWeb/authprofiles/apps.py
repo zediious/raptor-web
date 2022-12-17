a1bc8b55b03e9512a8502b65844abe8abd27f434
zediious <zediious@aol.com>
2022-12-17 02:47:24 -0500

    App verbose names

--- raptorWeb/authprofiles/apps.py
+++ raptorWeb/authprofiles/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AuthprofilesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'authprofiles'
+    verbose_name = 'User Profiles and Auth'
--- raptorWeb/staffapps/apps.py
+++ raptorWeb/staffapps/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class StaffappsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'staffapps'
+    verbose_name = 'Staff Applications'
