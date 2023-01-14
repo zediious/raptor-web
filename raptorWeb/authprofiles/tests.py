00d9fc47378ec287b3456b0deb3f41d7efa3838c
zediious <zediious@aol.com>
2023-01-14 15:20:01 -0500

    Authprofiles/staffapps very basic tests
    Just dipping into this, I plan to make these tests much more useful besides just creating/getting databse objects.

--- raptorWeb/authprofiles/tests.py
+++ raptorWeb/authprofiles/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from authprofiles.models import User, UserProfileInfo, DiscordUserInfo
 
-# Create your tests here.
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        DiscordUserInfo.objects.create(
+            id = '0',
+            tag='TestMan101#1945',
+            username='TestMan',
+            profile_picture='https://shadowraptor.net/media/profile_pictures/cyberraptor.jpg',
+            pub_flags = '0',
+            flags = '0',
+            locale='en',
+            mfa_enabled='False',
+            minecraft_username='TestMan',
+            favorite_modpack = 'TestPack')
+
+        user = User.objects.create(
+            email = 'test@testman.com',
+            username='TestMan102',
+            password='badpassword')
+
+        UserProfileInfo.objects.create(
+            user = user,
+            minecraft_username = 'TestMan2',
+            favorite_modpack = 'TestPack2')
+
+    def test_user_get(self):
+        default_user = User.objects.get(username = 'TestMan102')
+        UserProfileInfo.objects.get(user = default_user)
+        DiscordUserInfo.objects.get(username = 'TestMan')
--- raptorWeb/staffapps/tests.py
+++ raptorWeb/staffapps/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from staffapps.models import StaffApplication, ModeratorApplication, AdminApplication
 
-# Create your tests here.
+class StaffAppTestCase(TestCase):
+    def setUp(self):
+        ModeratorApplication.objects.create(
+            age = '12',
+            time = 'UTC all day',
+            mc_name = 'teststaffmod',
+            discord_name = 'teststaffmod#1234',
+            voice_chat = 'True',
+            description = 'I am a good staff member',
+            modpacks = 'I know many modpacks',
+            experience = 'I have much experience',
+            why_join = 'I want to be staff',
+            contact_uppers = 'I can contact uppers'
+        )
+
+        AdminApplication.objects.create(
+            age = '19',
+            time = 'UTC all day',
+            mc_name = 'teststaffadmin',
+            discord_name = 'teststaffadmin#1234',
+            voice_chat = 'True',
+            description = 'I am a good staff member',
+            modpacks = 'I know many modpacks',
+            experience = 'I have much experience',
+            why_join = 'I want to be staff',
+            plugins = 'I know all the plugins',
+            api = 'I am familiar with api',
+            it_knowledge = 'I am an IT god',
+            linux = 'I daily drive Arch',
+            ptero = 'I use Pterodactyl'
+        )
+
+    def test_user_get(self):
+        ModeratorApplication.objects.get(mc_name = 'teststaffmod')
+        AdminApplication.objects.get(mc_name = 'teststaffadmin')
