3d1b23d2b6777a5bb4a755963f71e016fe7d8423
zediious <zediious@aol.com>
2023-01-18 00:00:54 -0500

    announce/rules/banneditem/voting to gameservers
    Created HTMX views in gameservers that will return announcements, rules, banned items, and voting information for each server, within their same original accordions.
    
    Individual pages within raptormc still exist to then call these HTMX views. /rules contains Network Rules from raptormc's Informativetext's without gameservers at all.
    
    - adjusted size of SVG loading spinners to be match size
    - Added check to jobs.py to see if this was the first time the poll has run (first start). If it is, then it will run. This is done with a class variable in the player_poller/PlayerCounts object.

--- raptorWeb/gameservers/urls.py
+++ raptorWeb/gameservers/urls.py
@@ -9,6 +9,10 @@ urlpatterns = [
     path('server_button_poll/', views.Server_Buttons.as_view(), name="server_buttons_poll"),
     path('server_button_poll_loading/', views.Server_Buttons_Loading.as_view(), name="server_buttons_poll_loading"),
     path('server_modal_poll/', views.Server_Modals.as_view(), name="server_modals_poll"),
-    path('total_count_poll/', views.Total_Count.as_view(), name="totol_counts_poll")
+    path('total_count_poll/', views.Total_Count.as_view(), name="totol_counts_poll"),
+    path('server_rules_poll/', views.Server_Rules.as_view(), name="server_rules_poll"),
+    path('server_banned_items_poll/', views.Server_Banned_Items.as_view(), name="server_banned_items_poll"),
+    path('server_voting_poll/', views.Server_Voting.as_view(), name="server_voting_poll"),
+    path('server_announcements_poll/', views.Server_Announcements.as_view(), name="server_announcements_poll")
 
 ]
