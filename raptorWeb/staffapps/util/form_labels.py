81c52c87a0eed9cf7893ffea2db027d042ff4200
zediious <zediious@aol.com>
2023-01-17 23:08:57 -0500

    Fix form_labels.py
    Removed commas and backlashes that broke string formatting

--- raptorWeb/staffapps/util/form_labels.py
+++ raptorWeb/staffapps/util/form_labels.py
@@ -1,28 +1,24 @@
-ask_age = "How old are you?",
-age_admin = "You must be at least 18 years old to apply directly for admin",
-age_mod = "You must be at least 10 years old to apply",
-time_commitment = "What Timezone do you live in? How often and at what time of the day will you be available?",
-mc_name = "What is your Minecraft In-Game name?",
-discord_help = "Format it as such = Zediious#1234",
-mc_name_verify = "Confirm your Minecraft In-Game name.",
-discord_name = "What is your Discord Username?",
-discord_name_verify = "Confirm your Discord Username",
-voice_chat = "Do you have a working microphone and are comfortable speaking in Voice Chat?",
-description = "Give a general description of yourself, and how you operate.",
-contact_uppers = "Do you have the ability to quickly establish the problem, and get in contact with higher up if someone is breaking the rules and/or hacking?",
-plugin_help = "Our 1.16.5 servers use server-side mods rather than plugins.",
-server_api = "Are you familiar with/have used Minecraft reverse proxy software as well as the various APIs to integrate Forge with plugins (SpongeForge, Mohist, Magma)?",
-it_knowledge = "How much knowledge to do you have in terms of IT/networking/software development? If your profession includes either of these skills that is very useful information.",
-it_help = "Having a basic understanding of network concepts is the bare minimum.",
-linux = "How much experience and/or knowledge do you have regarding Linux system administration and general usage?",
-ptero = "Are you familiar with the Pterodactyl Game Server management panel and its usage?",
-ptero_help = "Having set up /configuring an instance of Pterodactyl yourself is not a requirement.",
-experience_help = "Any Admin roles you've had outside of Minecraft may also be described here.",
-why_join = "Why do you want to join the ShadowRaptor staff team?",
-experience = "Please provide as much information as you can about any Admin roles you've had on Minecraft servers, whether you owned the server or were staff. \
-    Describe what exactly it was you did for the server you were staff of in as much detail as possible.",
-modpack_knowledge = "Administrating a modded server requires good knowledge of how mods play, to properly identify certain things. \
-    Have you played the modpacks we run on our network, \and/or have a good understanding of how they/modded Minecraft in general works? \
-        Provide a short history of your time with Minecraft here as well.",
-plugin_knowledge = "Plugin/server-side mod knowledge is a necessity to become an Admin. Are there are any plugins in particular that you \
-    have extensive experience with? Do you believe that you can adapt to different configurations?",
+ask_age = "How old are you?"
+age_admin = "You must be at least 18 years old to apply directly for admin"
+age_mod = "You must be at least 10 years old to apply"
+time_commitment = "What Timezone do you live in? How often and at what time of the day will you be available?"
+mc_name = "What is your Minecraft In-Game name?"
+discord_help = "Format it as such = Zediious#1234"
+mc_name_verify = "Confirm your Minecraft In-Game name."
+discord_name = "What is your Discord Username?"
+discord_name_verify = "Confirm your Discord Username"
+voice_chat = "Do you have a working microphone and are comfortable speaking in Voice Chat?"
+description = "Give a general description of yourself, and how you operate."
+contact_uppers = "Do you have the ability to quickly establish the problem, and get in contact with higher up if someone is breaking the rules and/or hacking?"
+plugin_help = "Our 1.16.5 servers use server-side mods rather than plugins."
+server_api = "Are you familiar with/have used Minecraft reverse proxy software as well as the various APIs to integrate Forge with plugins (SpongeForge, Mohist, Magma)?"
+it_knowledge = "How much knowledge to do you have in terms of IT/networking/software development? If your profession includes either of these skills that is very useful information."
+it_help = "Having a basic understanding of network concepts is the bare minimum."
+linux = "How much experience and/or knowledge do you have regarding Linux system administration and general usage?"
+ptero = "Are you familiar with the Pterodactyl Game Server management panel and its usage?"
+ptero_help = "Having set up /configuring an instance of Pterodactyl yourself is not a requirement."
+experience_help = "Any Admin roles you've had outside of Minecraft may also be described here."
+why_join = "Why do you want to join the ShadowRaptor staff team?"
+experience = "Please provide as much information as you can about any Admin roles you've had on Minecraft servers, whether you owned the server or were staff. Describe what exactly it was you did for the server you were staff of in as much detail as possible."
+modpack_knowledge = "Administrating a modded server requires good knowledge of how mods play, to properly identify certain things. Have you played the modpacks we run on our network, and/or have a good understanding of how they/modded Minecraft in general works? Provide a short history of your time with Minecraft here as well."
+plugin_knowledge = "Plugin/server-side mod knowledge is a necessity to become an Admin. Are there are any plugins in particular that you have extensive experience with? Do you believe that you can adapt to different configurations?"
