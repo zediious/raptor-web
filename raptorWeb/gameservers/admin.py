facbbd3cc306278271232bcb5e011f1eac266872
zediious <zediious@aol.com>
2023-01-20 02:14:29 -0500

    Misc cleanup
    - Removed InformativeTextAdmin object from gameservers/admin.py
    - Added verbose app name for gameservers ("Game Servers"
    - Removed whitespace from old code in raptormc/views.py

--- raptorWeb/gameservers/admin.py
+++ raptorWeb/gameservers/admin.py
@@ -10,12 +10,6 @@ class ServerAdmin(admin.ModelAdmin):
         models.TextField: {'widget': Textarea(attrs={'rows':6, 'cols':60})},
     }
 
-class InformativeTextAdmin(admin.ModelAdmin):
-    formfield_overrides = {
-        models.CharField: {'widget': TextInput(attrs={'size':'35'})},
-        models.TextField: {'widget': Textarea(attrs={'rows':6, 'cols':60})},
-    }
-
 admin.site.register(Server, ServerAdmin)
 admin.site.register(PlayerCount)
 admin.site.register(PlayerName)
--- raptorWeb/gameservers/apps.py
+++ raptorWeb/gameservers/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class GameserversConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'gameservers'
+    verbose_name = 'Game Servers'
