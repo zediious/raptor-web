e7020391c740ad4ed67c1ebb88794428de0be5bd
zediious <zediious@aol.com>
2023-01-23 19:32:41 -0500

    raptorbot verbose name

--- raptorWeb/raptorbot/apps.py
+++ raptorWeb/raptorbot/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class RaptorbotConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'raptorbot'
+    verbose_name = 'Discord Bot'
