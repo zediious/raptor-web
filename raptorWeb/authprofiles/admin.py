f729122173197cc7dfbe5513d5f748530abeb011
zediious <zediious@aol.com>
2023-01-25 19:50:49 -0500

    init
    Working on getting default user registration set up with the new model. Only focusing on the back end (models being created correctly, etc) at the moment, I will modify templates/views to reflect correct data on the frontend afterwards.
    
    This is still in a primitive state. Work has not begun on integrating discord users

--- raptorWeb/authprofiles/admin.py
+++ raptorWeb/authprofiles/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.models import User, Group
 
-from raptorWeb.authprofiles.models import UserProfileInfo, DiscordUserInfo
+from raptorWeb.authprofiles.models import RaptorUser, UserProfileInfo, DiscordUserInfo
 
+admin.site.unregister(User)
+admin.site.unregister(Group)
+
+admin.site.register(RaptorUser)
 admin.site.register(UserProfileInfo)
 admin.site.register(DiscordUserInfo)
