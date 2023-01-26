4977810d60517beec844861551060277e890dcaf
zediious <zediious@aol.com>
2023-01-25 21:53:56 -0500

    init v2
    still working

--- raptorWeb/authprofiles/auth.py
+++ raptorWeb/authprofiles/auth.py
@@ -1,23 +1,49 @@
 from logging import getLogger
+from random import randint
 
 from django.contrib.auth.backends import BaseBackend
+from django.core.files.base import ContentFile
 
-from raptorWeb.authprofiles.models import DiscordUserInfo
+from requests import get
+
+from raptorWeb.authprofiles.models import RaptorUser, UserProfileInfo, DiscordUserInfo
 
 LOGGER = getLogger('raptorWeb.authprofiles.auth')
 
 class DiscordAuthBackend(BaseBackend):
 
     def authenticate(self, request, user):
-        find_user = DiscordUserInfo.objects.filter(id=user["id"])
-        if len(find_user) == 0:
-            LOGGER.info("No user found with Discord ID, creating now")
-            new_user = DiscordUserInfo.objects.create_new_discord_user(user)
+        try:
+            find_user = RaptorUser.objects.get(username=f'{user["username"]}#{user["discriminator"]}')
+            return find_user
+        except RaptorUser.DoesNotExist:
+            LOGGER.info("No user found with Username, creating now")
+            discord_tag = f'{user["username"]}#{user["discriminator"]}'
+            avatar_url = f'https://cdn.discordapp.com/avatars/{user["id"]}/{user["avatar"]}.png'
+            avatar_image = ContentFile(get(avatar_url).content)
+
+            new_discord_info = DiscordUserInfo.objects.create(
+                id = user["id"],
+                tag = discord_tag,
+                pub_flags = user["public_flags"],
+                flags = user["flags"],
+                locale = user["locale"],
+                mfa_enabled = user["mfa_enabled"],
+            )
+
+            new_extra_info = UserProfileInfo.objects.create(
+                profile_picture = avatar_image
+            )
+
+            new_user = RaptorUser.objects.create(
+                is_discord_user = True,
+                username = discord_tag.split('#')[0],
+                password = randint(100000000, 999999999),
+                user_profile_info = new_extra_info,
+                discord_user_info = new_discord_info
+            )
             return new_user
 
-        elif len(find_user) == 1:
-            LOGGER.debug(find_user.first())
-            return find_user
 
     def get_user(self, user_id):
 
