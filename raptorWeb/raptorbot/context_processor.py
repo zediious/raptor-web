54797f7d09c766da0989936b1c24dee53d548424
Jack <zediious@aol.com>
2023-02-03 21:33:17 -0500

    Make sure Guilds exist before iterating

--- raptorWeb/raptorbot/context_processor.py
+++ raptorWeb/raptorbot/context_processor.py
@@ -1,6 +1,9 @@
 from raptorWeb.raptorbot.models import DiscordGuild
 
 def add_discord_guild_data(request):
-    guilds = DiscordGuild.objects.all()
-    for guild in guilds:
-        return {"discord_guild": guild}
+    if DiscordGuild.objects.count() != 0:
+        guilds = DiscordGuild.objects.all()
+        for guild in guilds:
+            return {"discord_guild": guild}
+    else:
+        return {"no_discord_guild": "none"}
