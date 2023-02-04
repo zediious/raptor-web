18d1d9741a052e6f53b67fa11b3818892103a973
Jack <zediious@aol.com>
2023-02-03 21:32:48 -0500

    Correct createSuper managment command for new user

--- raptorWeb/raptormc/management/commands/createSuper.py
+++ raptorWeb/raptormc/management/commands/createSuper.py
@@ -1,23 +1,31 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
+from django.utils.text import slugify
 from django.conf import settings
 
-from raptorWeb.authprofiles.models import RaptorUser
+from raptorWeb.authprofiles.models import RaptorUser, UserProfileInfo
 
 ADMINS = getattr(settings, 'ADMINS')
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        if RaptorUser.objects.count() == 1:
+        if RaptorUser.objects.count() == 0:
             for user in ADMINS:
+                admin_info = UserProfileInfo.objects.create()
+                admin_info.save()
                 username = user[0].replace(' ', '')
                 email = user[1]
                 password = 'admin'
-                print('Creating account for %s (%s)' % (username, email))
                 admin = RaptorUser.objects.create_superuser(email=email, username=username, password=password)
+                admin.user_slug = slugify(username)
+                admin.user_profile_info = admin_info
+                admin.is_discord_user = False
                 admin.is_active = True
                 admin.is_admin = True
+                admin.is_staff = True
+                admin.is_superuser = True
                 admin.save()
-                self.stdout.write("[INFO] Base superuser created. Change the credentials!", ending='')
+                self.stdout.write(f"[INFO] Base superuser created with username: {admin.username}. Change the credentials!", ending='')
+                
         else:
             self.stdout.write("[INFO] Base superuser can only be initialized if no Accounts exist", ending='')
