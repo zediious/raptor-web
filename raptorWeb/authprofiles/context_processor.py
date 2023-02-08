468c2763736122c2b69d9ac9b1f6aae4afe9e05b
zediious <zediious@aol.com>
2023-02-08 12:23:52 -0500

    Redo authprofiles context processor for RaptorUser

--- raptorWeb/authprofiles/context_processor.py
+++ raptorWeb/authprofiles/context_processor.py
@@ -1,18 +1,9 @@
 from django.conf import settings
 
-from raptorWeb.authprofiles.models import UserProfileInfo, DiscordUserInfo
+from raptorWeb.authprofiles.models import RaptorUser
 
 BASE_USER_URL = getattr(settings, 'BASE_USER_URL')
 
 def all_users_to_context(request):
-    all_default_users = UserProfileInfo.objects.all()
-    all_discord_users = DiscordUserInfo.objects.all()
-    users_list = []
-
-    for default_user in all_default_users:
-        users_list.append(default_user)
-    for discord_user in all_discord_users:
-        users_list.append(discord_user)
-        
-    return {"current_members": users_list,
+    return {"current_members": RaptorUser.objects.all(),
             "base_user_url": BASE_USER_URL}
