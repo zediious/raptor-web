b128e6b447194157ec40f77f3782062428efc8ee
zediious <zediious@aol.com>
2023-02-08 18:36:15 -0500

    StaffApp model abstract, admin fields uneditable
    - Made the StaffApp model, which AdminApp/ModApp inherit from, abstract. Cleared migrations since this is breaking
    - Made changesto staffapps admin view to prevent editing of application fields, and hide backend fields.

--- raptorWeb/staffapps/admin.py
+++ raptorWeb/staffapps/admin.py
@@ -2,5 +2,68 @@ from django.contrib import admin
 
 from raptorWeb.staffapps.models import ModeratorApplication, AdminApplication
 
-admin.site.register(AdminApplication)
-admin.site.register(ModeratorApplication)
+class AdminAppAdmin(admin.ModelAdmin):
+    fields = (
+        'age',
+        'time',
+        'mc_name',
+        'discord_name',
+        'voice_chat',
+        'description',
+        'modpacks',
+        'experience',
+        'why_join',
+        'plugins',
+        'api',
+        'it_knowledge',
+        'linux',
+        'ptero'
+    )
+    
+    readonly_fields = (
+        'age',
+        'time',
+        'mc_name',
+        'discord_name',
+        'voice_chat',
+        'description',
+        'modpacks',
+        'experience',
+        'why_join',
+        'plugins',
+        'api',
+        'it_knowledge',
+        'linux',
+        'ptero'
+    )
+
+class ModAppAdmin(admin.ModelAdmin):
+    fields = (
+        'age',
+        'time',
+        'mc_name',
+        'discord_name',
+        'voice_chat',
+        'description',
+        'modpacks',
+        'experience',
+        'why_join',
+        'contact_uppers'
+    )
+    
+    readonly_fields = (
+        'age',
+        'time',
+        'mc_name',
+        'discord_name',
+        'voice_chat',
+        'description',
+        'modpacks',
+        'experience',
+        'why_join',
+        'contact_uppers'
+    )
+
+
+admin.site.register(AdminApplication, AdminAppAdmin)
+admin.site.register(ModeratorApplication, ModAppAdmin)
