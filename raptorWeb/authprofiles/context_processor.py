ec81fbb12a1403df7adefa6cdc5095b16e4ab437
zediious <zediious@aol.com>
2023-02-08 12:32:22 -0500

    USER_PATH from views to context
    This was silly. Wrong key name was used in context processor, but was still being added in each view.

--- raptorWeb/authprofiles/context_processor.py
+++ raptorWeb/authprofiles/context_processor.py
@@ -6,4 +6,4 @@ BASE_USER_URL = getattr(settings, 'BASE_USER_URL')
 
 def all_users_to_context(request):
     return {"current_members": RaptorUser.objects.all(),
-            "base_user_url": BASE_USER_URL}
+            "user_path": BASE_USER_URL}
