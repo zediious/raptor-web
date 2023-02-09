d44efb6a0689bdbd2427b350c46f94a9ca5e69f6
zediious <zediious@aol.com>
2023-02-09 03:24:39 -0500

    Rudimentary user importing
    Will be used to import from v0.1 user models. Very basic, turnedo n using IMPORT_USERS = True in .env file
    
    Will create RaptorUsers from json.
    
    This will be phased out after initial import is completed.

--- raptorWeb/authprofiles/context_processor.py
+++ raptorWeb/authprofiles/context_processor.py
@@ -1,9 +1,76 @@
-from django.conf import settings
+from os.path import join
+from json import load
 
-from raptorWeb.authprofiles.models import RaptorUser
+from django.conf import settings
+from django.utils.timezone import now, localtime
+
+from raptorWeb.authprofiles.models import RaptorUser, UserProfileInfo, DiscordUserInfo
 
 BASE_USER_URL = getattr(settings, 'BASE_USER_URL')
+BASE_DIR = getattr(settings, 'BASE_DIR')
+IMPORT_USERS = getattr(settings, 'IMPORT_USERS')
 
 def all_users_to_context(request):
+    if IMPORT_USERS == True:
+        import_users()
     return {"current_members": RaptorUser.objects.all(),
             "user_path": BASE_USER_URL}
+
+def import_users():
+    default_users = load(open(join(BASE_DIR, 'normal_user_list.json')))
+    discord_users = load(open(join(BASE_DIR, 'discord_user_list.json')))
+
+    for user in default_users:
+        new_extra = UserProfileInfo.objects.create(
+            picture_changed_manually = False
+        )
+        try:
+            new_extra.minecraft_username = default_users[user]["user_profile_info"]["minecraft_username"],
+            new_extra.favorite_modpack = default_users[user]["user_profile_info"]["favorite_modpack"]
+        except:
+            new_extra.minecraft_username = "",
+            new_extra.favorite_modpack = ""
+        new_user = RaptorUser.objects.create(
+            is_discord_user = False,
+            email = default_users[user]["email"],
+            username = default_users[user]["username"],
+            user_slug = default_users[user]["user_slug"],
+            is_active = True,
+            date_joined = localtime(now()),
+            last_login = localtime(now()),
+            user_profile_info = new_extra
+        )
+        new_user.password = default_users[user]["password"]
+        new_user.save()
+
+    for user in discord_users:
+        new_extra = UserProfileInfo.objects.create(
+            picture_changed_manually = False
+        )
+        try:
+            new_extra.minecraft_username = discord_users[user]["user_profile_info"]["minecraft_username"],
+            new_extra.favorite_modpack = discord_users[user]["user_profile_info"]["favorite_modpack"]
+        except:
+            new_extra.minecraft_username = "",
+            new_extra.favorite_modpack = ""
+        new_discord = DiscordUserInfo.objects.create(
+            id = discord_users[user]["discord_user_info"]["id"],
+            tag = discord_users[user]["discord_user_info"]["tag"],
+            pub_flags = discord_users[user]["discord_user_info"]["pub_flags"],
+            flags = discord_users[user]["discord_user_info"]["flags"],
+            locale = discord_users[user]["discord_user_info"]["locale"],
+            mfa_enabled = discord_users[user]["discord_user_info"]["mfa_enabled"],
+            avatar_string = "defaultavatarstringtobereplaced"
+        )
+
+        new_user = RaptorUser.objects.create(
+            is_discord_user = True,
+            email = "none@gmail.com",
+            username = discord_users[user]["username"],
+            user_slug = discord_users[user]["user_slug"],
+            is_active = True,
+            date_joined = localtime(now()),
+            last_login = localtime(now()),
+            user_profile_info = new_extra,
+            discord_user_info = new_discord
+        )
