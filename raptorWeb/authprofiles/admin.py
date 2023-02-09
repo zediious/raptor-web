bff1ae81ab3fd0e3d6150f5a15a5b623094a950c
zediious <zediious@aol.com>
2023-02-08 19:00:17 -0500

    Authprofiles admin useability
    - Added a ModelAdmin inheriting from default UserAdmin to RaptorUser. This ModelAdmin has had all fields overriden except for Password, to retain django's nice password widget. RaptorUser attributes are now separated into different sections
    - Changed some fields to "readonly_fields" that are not meant to be edited manually
    - Added search parameters for RaptorUser
    - Modified attributes used to sort when viewing all RaptorUsers
    - All fields for authprofiles models have been given descriptive help_text attributes as well as verbose_name, to make navigating admin quite a bit easier.

--- raptorWeb/authprofiles/admin.py
+++ raptorWeb/authprofiles/admin.py
@@ -1,11 +1,66 @@
+from django import forms
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
+from django.contrib.auth.admin import UserAdmin
 
 from raptorWeb.authprofiles.models import RaptorUser, UserProfileInfo, DiscordUserInfo
+
+class RaptorUserAdminForm(forms.ModelForm):
+    class Meta:
+        model = RaptorUser
+        widgets = {
+            'password': forms.PasswordInput()
+        }
+        fields = '__all__'
+
+class RaptorUserAdmin(UserAdmin):
+    fieldsets = (
+        ('General', {
+            'fields': (
+                'user_profile_info',
+                'discord_user_info',
+                'is_active',
+                'is_discord_user',
+                'email',
+                'first_name',
+                'last_name',
+                'username',
+                'user_slug',
+                'date_joined',
+                'last_login')
+        }),
+        ('Permissions', {
+            'classes': ('collapse',),
+            'fields': ('user_permissions','groups')
+        }),
+        ('Sensitive', {
+            'classes': ('collapse',),
+            'fields': ('is_superuser', 'is_staff', 'password')
+        })
+    )
+
+    readonly_fields = (
+        'is_discord_user',
+        'date_joined',
+        'last_login',
+        'user_profile_info',
+        'discord_user_info'
+    )
+
+    search_fields = [
+        'username',
+        'user_slug',
+        'email',
+        'discord_user_info__tag'
+    ]
+
+    list_display = ['username', 'email', 'is_discord_user', 'is_staff', 'is_active', 'date_joined']
+
+    filter_horizontal = ('user_permissions', 'groups')
 
 admin.site.unregister(User)
 admin.site.unregister(Group)
 
-admin.site.register(RaptorUser)
+admin.site.register(RaptorUser, RaptorUserAdmin)
 admin.site.register(UserProfileInfo)
 admin.site.register(DiscordUserInfo)
--- raptorWeb/authprofiles/models.py
+++ raptorWeb/authprofiles/models.py
@@ -8,23 +8,40 @@ class DiscordUserInfo(models.Model):
     Discord Information for a User that was registered using Discord OAuth2.
     """
     id = models.BigIntegerField(
-        primary_key=True)
+        primary_key=True,
+        help_text="Discord user's ID returned from Discord, also acts as model primary key.",
+        verbose_name="ID")
 
     tag = models.CharField(
-        max_length=100) 
+        max_length=100,
+        help_text="Combination of Discord Username and Discriminator, separated by a # sign.",
+        verbose_name="Discord Tag") 
 
-    pub_flags = models.IntegerField()
-
-    flags = models.IntegerField()
-
-    locale = models.CharField(
-        max_length=100
+    pub_flags = models.IntegerField(
+        help_text="Public flags for this Discord user returned from Discord.",
+        verbose_name="Public Flags"
     )
 
-    mfa_enabled = models.BooleanField()
+    flags = models.IntegerField(
+        help_text="Flags for this Discord user returned from Discord.",
+        verbose_name="Flags"
+    )
+
+    locale = models.CharField(
+        max_length=100,
+        help_text="Discord user's locale.",
+        verbose_name="Locale"
+    )
+
+    mfa_enabled = models.BooleanField(
+        help_text="Whether this user has multi-factor authentication enabled on their Discord account.",
+        verbose_name="MFA Enabled"
+    )
 
     avatar_string = models.CharField(
         max_length=200,
+        help_text="String returned from Discord, for fetching Avatar image.",
+        verbose_name="Avatar String",
         null=True
     )
 
@@ -41,20 +58,28 @@ class UserProfileInfo(models.Model):
     """
     picture_changed_manually = models.BooleanField(
         default=False,
-        null=True
+        null=True,
+        help_text="Indicates whether a user has manually changed their profile picture or not.",
+        verbose_name="Picture has been changed manually"
     )
 
     profile_picture = models.ImageField(
-        upload_to='profile_pictures', 
+        upload_to='profile_pictures',
+        help_text="A user's profile picture, uploaded and stored locally.",
+        verbose_name="Profile Picture",
         blank=True)
 
     minecraft_username = models.CharField(
         max_length=50,
+        help_text="A user's Minecraft Username.",
+        verbose_name="Minecraft Username",
         blank=True
     )
 
     favorite_modpack = models.CharField(
-        max_length=80, 
+        max_length=80,
+        help_text="A user's favorite Minecraft modpack.",
+        verbose_name="Favorite Modpack",
         blank=True
     )
     def __str__(self):
@@ -70,12 +95,15 @@ class RaptorUser(User):
     and DiscordUserInfo Model. Inherits from default Django user.
     """
     user_slug = models.SlugField(
-        null=True
+        null=True,
+        help_text="A user's username that has been converted to a slug/URL friendly format.",
+        verbose_name="User Slug"
     )
     
     is_discord_user=  models.BooleanField(
-        blank=True,
-        null=True
+        default=False,
+        help_text="Indicates whether a user registered their account using Discord.",
+        verbose_name="Is a Discord User"
     )
 
     user_profile_info = models.OneToOneField(
@@ -83,6 +111,8 @@ class RaptorUser(User):
         null=True,
         blank=True,
         related_name='profileinfo',
+        help_text="A User's extra profile information, stored in separate model.",
+        verbose_name="User Profile Information",
         on_delete=models.CASCADE)
 
     discord_user_info = models.OneToOneField(
@@ -90,6 +120,8 @@ class RaptorUser(User):
         null=True,
         blank=True,
         related_name='discordinfo',
+        help_text="A User's Discord user information, stored in a separate model. This field will/should not be populated for users that did not register with Discord.",
+        verbose_name="Discord User Information",
         on_delete=models.CASCADE)
 
     def get_profile_info(self):
