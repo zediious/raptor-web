9b2a0888c60516584e02e8687065db19fd72645b
zediious <zediious@aol.com>
2023-02-12 17:49:37 -0500

    Some typehints for gameservers

--- raptorWeb/gameservers/templatetags/serverContext.py
+++ raptorWeb/gameservers/templatetags/serverContext.py
@@ -1,10 +1,12 @@
+from typing import Optional
+
 from django import template
 
 from raptorWeb.gameservers.models import Server
 
-register = template.Library()
+register: template.Library = template.Library()
 
-def find_server(server_count, initial_value, value_to_find):
+def find_server(server_count: int, initial_value: dict, value_to_find: str) -> Optional[str]:
     """
     Used by all serverContext filters to find nested values in context
     """
@@ -15,112 +17,112 @@ def find_server(server_count, initial_value, value_to_find):
             continue
 
 @register.filter
-def get_key(value):
+def get_key(value: dict) -> str:
     """
     Get key from context dictionary.
     """
     return find_server(Server.objects.count(), value, "key")
 
 @register.filter
-def get_state(value):
+def get_state(value: dict) -> str:
     """
     Get server state from context dictionary.
     """
     return find_server(Server.objects.count(), value, "state")
 
 @register.filter
-def get_maintenance(value):
+def get_maintenance(value: dict) -> str:
     """
     Get maintenance status from context dictionary.
     """
     return find_server(Server.objects.count(), value, "maintenance")
 
 @register.filter
-def get_address(value):
+def get_address(value: dict) -> str:
     """
     Get server address from context dictionary.
     """
     return find_server(Server.objects.count(), value, "address")
 
 @register.filter
-def get_player_names(value):
+def get_player_names(value: dict) -> str:
     """
     Get list of player names from context dictionary.
     """
     return find_server(Server.objects.count(), value, "names")
 
 @register.filter
-def get_modpack_name(value):
+def get_modpack_name(value: dict) -> str:
     """
     Get modpack name from context dictionary.
     """
     return find_server(Server.objects.count(), value, "modpack_name")
 
 @register.filter
-def get_modpack_desc(value):
+def get_modpack_desc(value: dict) -> str:
     """
     Get modpack description from context dictionary.
     """
     return find_server(Server.objects.count(), value, "modpack_description")
 
 @register.filter
-def get_server_desc(value):
+def get_server_desc(value: dict) -> str:
     """
     Get server description from context dictionary.
     """
     return find_server(Server.objects.count(), value, "server_description")
 
 @register.filter
-def get_modpack_url(value):
+def get_modpack_url(value: dict) -> str:
     """
     Get modpack page url from context dictionary.
     """
     return find_server(Server.objects.count(), value, "modpack")
 
 @register.filter
-def get_rules(value):
+def get_rules(value: dict) -> str:
     """
     Get server rules from context dictionary.
     """
     return find_server(Server.objects.count(), value, "server_rules")
 
 @register.filter
-def get_banned_items(value):
+def get_banned_items(value: dict) -> str:
     """
     Get server banned items from context dictionary.
     """
     return find_server(Server.objects.count(), value, "server_banned_items")
 
 @register.filter
-def get_vote_links(value):
+def get_vote_links(value: dict) -> str:
     """
     Get server vote links from context dictionary.
     """
     return find_server(Server.objects.count(), value, "server_vote_links")
 
 @register.filter
-def get_modpack_version(value):
+def get_modpack_version(value: dict) -> str:
     """
     Get modpack version from context dictionary.
     """
     return find_server(Server.objects.count(), value, "modpack_version")
 
 @register.filter
-def get_player_count(value):
+def get_player_count(value: dict) -> str:
     """
     Get a server's player count from context dictionary.
     """
     return find_server(Server.objects.count(), value, "player_count")
 
 @register.filter
-def get_announcement_count(value):
+def get_announcement_count(value: dict) -> str:
     """
     Get a server's announcement count from context dictionary.
     """
     return find_server(Server.objects.count(), value, "announcement_count")
 
 @register.filter
-def get_modpack_image(value):
+def get_modpack_image(value: dict) -> str:
     """
     Get a server's modpack image from context dictionary
     """
--- raptorWeb/gameservers/util/playerCounts.py
+++ raptorWeb/gameservers/util/playerCounts.py
@@ -1,38 +1,39 @@
 from django.conf import settings
 
 from mcstatus import JavaServer
+from mcstatus.querier import QueryResponse
 
-LOCK_FILE_PATH = getattr(settings, 'LOCK_FILE_PATH')
+LOCK_FILE_PATH: str = getattr(settings, 'LOCK_FILE_PATH')
             
 class PlayerCounts():
     """
     Object containing data structures and methods used for polling
     ShadowRaptor Minecraft servers for state and player information.
     """
-    has_run = False
+    has_run: bool = False
     # Dictionary containing address/port/key names of servers
-    server_data = {}
+    server_data: dict = {}
     # Dict to track player counts and names in each server internally
-    currentPlayers = {}
+    currentPlayers: dict = {}
     # Template context dictionary
-    currentPlayers_DB = {
+    currentPlayers_DB: dict = {
         "server_info": [],
         "totalCount": 0
     }     
 
-    def _parse_key (self, ADDRESS):
+    def _parse_key (self, ADDRESS: str) -> str:
         """
         Returns a string that represents a key in the "currentPlayers" 
         Dictionary, gathered from domain name "ADDRESS" parameter.
         """
         return str(ADDRESS.split(".")[0])
 
-    def request_info(self, ADDRESS, PORT, KEY, do_query = True):
+    def _request_info(self, ADDRESS: str, PORT: int, KEY: str, do_query: bool = True) -> None:
         """
         Sets the "count", "names" and "online" keys within the provided "KEY" parameter
         to values gathered from a domain name "ADDRESS" parameter.
         """
-        def _load_offline_server(KEY, ADDRESS):
+        def _load_offline_server(KEY: str, ADDRESS: str) -> None:
             """
             Helper function to update currentPlayers with
             information if a server is offline or is not to
@@ -48,9 +49,8 @@ class PlayerCounts():
                 })
 
         if do_query == True:
-
             try:
-                serverJSON = JavaServer(ADDRESS, PORT).query()
+                serverJSON: QueryResponse = JavaServer(ADDRESS, PORT).query()
                 self.currentPlayers["totalCount"] += serverJSON.players.online
                 self.currentPlayers.update({
                     KEY: {
@@ -67,7 +67,7 @@ class PlayerCounts():
         else:
             _load_offline_server(KEY, ADDRESS)
 
-    def get_current_players(self):
+    def get_current_players(self) -> dict:
         """
         Return a dictionary containing total player count, as well as nested dictionaries
         with specific counts, player names and states for each server
@@ -77,15 +77,16 @@ class PlayerCounts():
         }
 
         for server in self.server_data:
+            if (self.server_data[server]["do_query"] == False
+            or self.server_data[server]["is_default"] == True):
+                self._request_info(
+                    self.server_data[server]["address"], 
+                    self.server_data[server]["port"], 
+                    self._parse_key(self.server_data[server]["address"]),
+                    do_query = False)
 
-            if self.server_data[server]["do_query"] == False or self.server_data[server]["is_default"] == True:
-                self.request_info(
-                self.server_data[server]["address"], 
-                self.server_data[server]["port"], 
-                self._parse_key(self.server_data[server]["address"]),
-                do_query = False)
             else:
-                self.request_info(
+                self._request_info(
                     self.server_data[server]["address"], 
                     self.server_data[server]["port"], 
                     self._parse_key(self.server_data[server]["address"]))
