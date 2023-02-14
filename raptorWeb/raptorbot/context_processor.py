f9d7d5043fb1c1f9bd5a9333af56bcfab87500e3
zediious <zediious@aol.com>
2023-02-14 03:03:34 -0500

    raptorbot context processor get guild from .env ID

--- raptorWeb/raptorbot/context_processor.py
+++ raptorWeb/raptorbot/context_processor.py
@@ -1,9 +1,12 @@
+from django.conf import settings
+
 from raptorWeb.raptorbot.models import DiscordGuild
 
+DISCORD_GUILD = getattr(settings, 'DISCORD_GUILD')
+
 def add_discord_guild_data(request):
-    if DiscordGuild.objects.count() != 0:
-        guilds = DiscordGuild.objects.all()
-        for guild in guilds:
-            return {"discord_guild": guild}
-    else:
+    try:
+        return {"discord_guild": DiscordGuild.objects.get(guild_id=DISCORD_GUILD)}
+
+    except DiscordGuild.DoesNotExist:
         return {"no_discord_guild": "none"}
