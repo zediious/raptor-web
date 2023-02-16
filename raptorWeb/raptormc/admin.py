77591f3e9cc5c4624b5f2ed59ef6d2237dc39a49
zediious <zediious@aol.com>
2023-02-16 01:46:21 -0500

    Site information from model
    These will be modified during the first startup wizard. Define the following in this model
    - Brand name
    - main/secondary colors
    - Brand image
    - Background image
    
    - Prevent creating new SiteInformations, ServerStatistics, and InformativeTexts in the admin
    - Define ModelAdmin for InformativeText

--- raptorWeb/raptormc/admin.py
+++ raptorWeb/raptormc/admin.py
@@ -5,7 +5,8 @@ from django.db import models
 
 from tinymce.widgets import TinyMCE
 
-from raptorWeb.raptormc.models import InformativeText
+from raptorWeb.raptormc.models import InformativeText, SiteInformation
+
 
 class InformativeTextAdminForm(ModelForm):
     class Meta:
@@ -15,6 +16,80 @@ class InformativeTextAdminForm(ModelForm):
         }
         fields = '__all__'
 
+class SiteInformationAdminForm(ModelForm):
+    class Meta:
+        model = SiteInformation
+        widgets = {
+            'main_color': TextInput(attrs={'type': 'color'}),
+            'secondary_color': TextInput(attrs={'type': 'color'})
+        }
+        fields = '__all__'
+
+
 class InformativeTextAdmin(admin.ModelAdmin):
+    """
+    Object defining behavior and display of 
+    InformativeTexts in the Django admin interface.
+    """
     form = InformativeTextAdminForm
+
+    fieldsets: tuple[tuple[str, dict[str, tuple[str]]]] = (
+        ('Informative Text', {
+            'fields': (
+                'enabled',
+                'name',
+                'content')
+        }),
+    )
+
+    readonly_fields: tuple[str] = (
+        'name',
+    )
+
+    search_fields: list[str] = [
+        'name',
+    ]
+
+    list_display: list[str] = ['name', 'enabled']
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+
+class SiteInformationAdmin(admin.ModelAdmin):
+    """
+    Object defining behavior and display of 
+    SiteInformations in the Django admin interface.
+    """
+    form = SiteInformationAdminForm
+
+    fieldsets: tuple[tuple[str, dict[str, tuple[str]]]] = (
+        ('Information', {
+            'fields': (
+                'brand_name',)
+        }),
+        ('Colors', {
+            'fields': (
+                'main_color',
+                'secondary_color')
+        }),
+        ('Images', {
+            'fields': (
+                'branding_image',
+                'background_image')
+        })
+    )
+
+    list_display: list[str] = [
+        'brand_name',
+        'main_color',
+        'secondary_color',
+        'branding_image',
+        'background_image']
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+
 admin.site.register(InformativeText, InformativeTextAdmin)
+admin.site.register(SiteInformation, SiteInformationAdmin)
--- raptorWeb/raptormc/context_processor.py
+++ raptorWeb/raptormc/context_processor.py
@@ -1,13 +1,16 @@
 from django.conf import settings
 
+from raptorWeb.raptormc.models import SiteInformation
+
 DOMAIN_NAME = getattr(settings, 'DOMAIN_NAME')
 DEFAULT_MEDIA = getattr(settings, 'DEFAULT_MEDIA')
 WEB_PROTO = getattr(settings, 'WEB_PROTO')
 
 def context_process(request):
-    context_addition = {
-        "pub_domain": DOMAIN_NAME,
-        "web_proto": WEB_PROTO,
-        "default_media": DEFAULT_MEDIA
-    }
-    return context_addition
+    site_info = SiteInformation.objects.get_or_create(pk=0)[0]
+    return {
+            "pub_domain": DOMAIN_NAME,
+            "web_proto": WEB_PROTO,
+            "default_media": DEFAULT_MEDIA,
+            "site_info_model": site_info
+        }
