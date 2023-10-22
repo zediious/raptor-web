815db43521ad95e24856f03d474d9b4c6ae2bf4e
zediious <zediious@aol.com>
2023-10-22 14:55:35 -0400

    rewrite raptormc urls for htmx access

--- raptorWeb/raptormc/urls.py
+++ raptorWeb/raptormc/urls.py
@@ -1,4 +1,4 @@
-from django.urls import URLPattern, path
+from django.urls import URLPattern, path, re_path
 
 from raptorWeb.raptormc import views
 from raptorWeb.raptormc.exchange import current_urlpatterns
@@ -7,31 +7,26 @@ app_name: str = "raptormc"
 
 urlpatterns: list[URLPattern] = [
 
-    path('', views.HomeServers.as_view(), name="home"),
+    path('raptormc/api/html/home', views.HomeServers.as_view(), name="home"),
     # Information
-    path('announcements', views.Announcements.as_view(), name="announcements"),
-    path('announcements/', views.Announcements.as_view(), name="announcements_slash"),
-    path('rules', views.Rules.as_view(), name="rules"),
-    path('rules/', views.Rules.as_view(), name="rules"),
-    path('banneditems', views.BannedItems.as_view(), name="banned_items"),
-    path('banneditems/', views.BannedItems.as_view(), name="banned_items_slash"),
-    path('voting', views.Voting.as_view(), name="voting"),
-    path('voting/', views.Voting.as_view(), name="voting_slash"),
-    path('howtojoin', views.HowToJoin.as_view(), name="joining"),
-    path('howtojoin/', views.HowToJoin.as_view(), name="joining_slash"),
-    path('applications', views.StaffApps.as_view(), name="staff_apps"),
-    path('applications/', views.StaffApps.as_view(), name="staff_apps_slash"),
+    path('raptormc/api/html/announcements', views.Announcements.as_view(), name="announcements"),
+    path('raptormc/api/html/rules', views.Rules.as_view(), name="rules"),
+    path('raptormc/api/html/banneditems', views.BannedItems.as_view(), name="banneditems"),
+    path('raptormc/api/html/voting', views.Voting.as_view(), name="voting"),
+    path('raptormc/api/html/howtojoin', views.HowToJoin.as_view(), name="howtojoin"),
+    path('raptormc/api/html/applications', views.StaffApps.as_view(), name="applications"),
     # Created Pages
-    path('pages/<str:page_name>', views.PageView.as_view(), name="pages"),
+    path('raptormc/api/html/pages/<str:page_name>', views.PageView.as_view(), name="pages"),
     # Users
-    path('user/', views.SiteMembers.as_view(), name='site_members'),
-    path('user/<str:profile_name>', views.User_Page.as_view(), name="user_page"),
-    path('user/reset/<str:profile_name>/<str:user_reset_token>', views.User_Pass_Reset.as_view(), name="user_reset_pass"),
+    path('raptormc/api/html/user', views.SiteMembers.as_view(), name='user'),
+    path('raptormc/api/html/user/<str:profile_name>', views.User_Page.as_view(), name="user_page"),
+    path('raptormc/api/html/user/reset/<str:profile_name>/<str:user_reset_token>', views.User_Pass_Reset.as_view(), name="user_reset_pass"),
     # Admin Panel
     path('panel/', views.Admin_Panel.as_view(), name='admin_panel_base'),
     # 404
     path('404', views.View_404.as_view(), name='404_view'),
     # API
+    path('raptormc/api/action/session/headerbox/update', views.Update_Headerbox_State.as_view(), name='update_headerbox_state'),
     path('api/session/headerbox/update', views.Update_Headerbox_State.as_view(), name='update_headerbox_state')
 
 ]
