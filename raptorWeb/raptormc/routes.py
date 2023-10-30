9150cdc0b3a8cf0f335bf345948c8021dedc1664
zediious <zediious@aol.com>
2023-10-29 21:46:49 -0400

    Properly get users without profile picture

--- raptorWeb/raptormc/routes.py
+++ raptorWeb/raptormc/routes.py
@@ -99,12 +99,18 @@ def check_route(request):
         path = request.path[:first_slash]+request.path[first_slash+1:]
         if (str(route.name) == str(path)
         or str(f'{route.name}/') == str(path)):
+            
             if route.user != None:
+                try:
+                    user_avatar = route.user.user_profile_info.profile_picture.url
+                except ValueError:
+                    user_avatar = site_info.avatar_image.url
+                    
                 return {
                     "og_user": route.user,
                     "og_color": site_info.main_color,
                     "og_url": f"{WEB_PROTO}://{DOMAIN_NAME}/{BASE_USER_URL}/{route.user.user_slug}",
-                    "og_image": f"{WEB_PROTO}://{DOMAIN_NAME}/{route.user.user_profile_info.profile_picture.url}",
+                    "og_image": f"{WEB_PROTO}://{DOMAIN_NAME}/{user_avatar}",
                     "og_title": f"{site_info.brand_name} | {route.user.username}",
                     "og_desc": f"User Profile for {route.user.username}"
                 }
