6d964235760d1d94ae9c20827fde0b592a5d7c38
zediious <zediious@aol.com>
2023-12-17 17:17:52 -0500

    remove debug task

--- config/celery.py
+++ config/celery.py
@@ -12,7 +12,3 @@ app = Celery('raptor', broker='pyamqp://guest@rabbitmq//')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
-
-@app.task(bind=True, ignore_result=True)
-def debug_task(self):
-    LOGGER.debug(f'Request: {self.request!r}')
