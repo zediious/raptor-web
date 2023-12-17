87259807d3ddf4428ce93274fdf66817f39e1bdc
zediious <zediious@aol.com>
2023-12-17 09:29:24 -0500

    additional donation module logging

--- config/logging.py
+++ config/logging.py
@@ -160,6 +160,11 @@ LOGGING_DEFINITION = {
             'level': 'DEBUG',
             'propagate': False,
         },
+        'donations.donationTags': {
+            'handlers': ['console', 'log_file', 'mail_admins'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
         'raptorbot.models': {
             'handlers': ['console', 'bot_log_file', 'mail_admins'],
             'level': 'DEBUG',
