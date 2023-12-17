90a3b172b545fe10c70c8906f60eb65aacc00efc
zediious <zediious@aol.com>
2023-12-17 11:48:06 -0500

    forms for entering discord username

--- raptorWeb/donations/forms.py
+++ raptorWeb/donations/forms.py
@@ -4,3 +4,17 @@ class SubmittedDonationForm(forms.Form):
     minecraft_username = forms.CharField(
         required=True,
     )
+    
+    discord_username = forms.CharField(
+        help_text=("This is not required, but you must enter a Discord "
+                   "username if you want to receive Roles on Discord."),
+        required=False,
+    )
+    
+    
+class DonationDiscordUsernameForm(forms.Form):
+    discord_username = forms.CharField(
+        help_text=("This is not required, but you must enter a Discord "
+                   "username if you want to receive Roles on Discord."),
+        required=False,
+    )
