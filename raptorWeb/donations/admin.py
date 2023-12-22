e2cfca48897be4ff5c9245d699367066b9a99ba6
zediious <zediious@aol.com>
2023-12-21 20:18:40 -0500

    Remove CompletedDonation modeladmin

--- raptorWeb/donations/admin.py
+++ raptorWeb/donations/admin.py
@@ -3,7 +3,7 @@ from django.forms import ModelForm
 
 from tinymce.widgets import TinyMCE
 
-from raptorWeb.donations.models import DonationPackage, DonationServerCommand, DonationDiscordRole, CompletedDonation
+from raptorWeb.donations.models import DonationPackage, DonationServerCommand, DonationDiscordRole
 
 class DonationPackageAdminForm(ModelForm):
     class Meta:
@@ -12,52 +12,6 @@ class DonationPackageAdminForm(ModelForm):
             'package_description': TinyMCE
         }
         fields = '__all__'
-        
-        
-class CompletedDonationAdmin(admin.ModelAdmin):
-    """
-    Object defining behavior and display of 
-    Completed Donations in the Django admin interface.
-    """
-    fieldsets: tuple[tuple[str, dict[str, tuple[str]]]] = (
-        ('Donation Information', {
-            'fields': (
-                'donation_datetime',
-                'donating_user',
-                'minecraft_username',
-                'discord_username',
-                'bought_package',
-                'spent',
-                'session_id',
-                'checkout_id',
-                'completed',
-                'sent_commands_count',
-                'gave_roles_count')
-        }),
-    )
-
-    search_fields: list[str] = [
-        'minecraft_username',
-        'discord_username',
-        'donating_user',
-        'bought_package'
-    ]
-
-    list_display: list[str] = [
-        'minecraft_username',
-        'discord_username',
-        'spent',
-        'bought_package',
-        'sent_commands_count',
-        'gave_roles_count',
-        'completed'  
-    ]
-    
-    def has_add_permission(self, request, obj=None):
-        return False
-
-    def has_change_permission(self, request, obj=None):
-        return False
 
 
 class DonationPackageAdmin(admin.ModelAdmin):
@@ -134,4 +88,3 @@ class DonationDiscordroleAdmin(admin.ModelAdmin):
 admin.site.register(DonationPackage, DonationPackageAdmin)
 admin.site.register(DonationServerCommand, DonationServerCommandAdmin)
 admin.site.register(DonationDiscordRole, DonationDiscordroleAdmin)
-admin.site.register(CompletedDonation, CompletedDonationAdmin)
