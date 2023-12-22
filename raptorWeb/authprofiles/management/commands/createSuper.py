ad8a4de0e391f70ca4ae66e0b4c97667bef58242
zediious <zediious@aol.com>
2023-12-21 22:34:02 -0500

    Properly create new superuser

--- raptorWeb/authprofiles/management/commands/createSuper.py
+++ raptorWeb/authprofiles/management/commands/createSuper.py
@@ -10,22 +10,22 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if RaptorUser.objects.count() == 0:
-            for user in ADMINS:
-                admin_info = UserProfileInfo.objects.create()
-                admin_info.save()
-                username = user[0].replace(' ', '')
-                email = user[1]
-                password = 'admin'
-                admin = RaptorUser.objects.create_superuser(email=email, username=username, password=password)
-                admin.user_slug = slugify(username)
-                admin.user_profile_info = admin_info
-                admin.is_discord_user = False
-                admin.is_active = True
-                admin.is_admin = True
-                admin.is_staff = True
-                admin.is_superuser = True
-                admin.save()
-                self.stdout.write(f"[INFO] Base superuser created with username: {admin.username}. Change the credentials!", ending='')
-                
+            default_admin = ADMINS[0]
+            admin_info = UserProfileInfo.objects.create()
+            admin_info.save()
+            username = default_admin[0].replace(' ', '')
+            email = default_admin[1]
+            password = 'admin'
+            admin = RaptorUser.objects.create_superuser(email=email, username=username, password=password)
+            admin.user_slug = slugify(username)
+            admin.user_profile_info = admin_info
+            admin.is_discord_user = False
+            admin.is_active = True
+            admin.is_admin = True
+            admin.is_staff = True
+            admin.is_superuser = True
+            admin.save()
+            self.stdout.write(f"[INFO] Base superuser created with username: {admin.username}. Change the credentials!", ending='')
+            
         else:
             self.stdout.write("[INFO] Base superuser can only be initialized if no Accounts exist", ending='')
