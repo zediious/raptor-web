209613c80744ad4da948bfcb67d83cfba1e7dc43
zediious <zediious@aol.com>
2024-04-27 17:57:22 -0400

    Remove unused constants

--- raptorWeb/authprofiles/templatetags/authTags.py
+++ raptorWeb/authprofiles/templatetags/authTags.py
@@ -7,11 +7,6 @@ from django.conf import settings
 LOGGER = getLogger('authprofiles.authTags')
 register = template.Library()
 
-DOMAIN_NAME: str = getattr(settings, 'DOMAIN_NAME')
-WEB_PROTO: str = getattr(settings, 'WEB_PROTO')
-BASE_USER_URL: str = getattr(settings, 'BASE_USER_URL')
-BASE_USER_URL_NUM: int = (len(BASE_USER_URL) + 1)
-
 
 @register.filter
 def get_user_from_url(value: str) -> str:
