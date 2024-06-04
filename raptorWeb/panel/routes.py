04469ebeed6cdcb656723dee3f4cdd01bf6092f9
Zediious <zediious@aol.com>
2024-06-03 20:46:21 -0400

    panel routing
    Add routes for server update pages
    
    Further add checks for "_IR" in template names, and ignore these/do not add a route for them if they exist.
    
    This is used to prevent base URLS for routes such as update without a parameter from being loaded as a valid route, which breaks the router when someone accessed it/displays a bad 404 page

--- raptorWeb/panel/routes.py
+++ raptorWeb/panel/routes.py
@@ -5,6 +5,7 @@ from django.utils.html import strip_tags
 from django.conf import settings
 
 from raptorWeb.raptormc.routes import Route
+from raptorWeb.gameservers.models import Server
 
 LOGGER = getLogger('raptormc.routes')
 DOMAIN_NAME: str = getattr(settings, 'DOMAIN_NAME')
@@ -22,13 +23,31 @@ def check_route(request):
         Iterate current URLPatterns and create
         Routes for each.
         """             
-        for pattern in CURRENT_URLPATTERNS[0]: 
+        for pattern in CURRENT_URLPATTERNS[0]:
+                if '_IR' in pattern.name:
+                    continue
+
                 current_routes.append(
                     Route(
                         name=f'panel/{pattern.name}',
                         route_type="main"
                     )
                 )
+
+    def _get_server_routes():
+        """
+        Iterate all servers and create a Route for
+        each server's update page.
+        """
+        all_servers = Server.objects.filter(archived=False)
+        for server in all_servers:
+            current_routes.append(
+                Route(
+                    name=f'panel/server/update/{server.pk}',
+                    route_type="server",
+                    server=server,
+                )
+            )
     
     # If request is to root path, we do not need to check routes 
     if request.path == '/panel/':
@@ -37,6 +56,7 @@ def check_route(request):
     current_routes: list = []
     
     _get_main_routes()
+    _get_server_routes()
     
     for route in current_routes:
         first_slash = request.path.index('/')
