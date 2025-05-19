bc9d634faf44e826f2cb5f73e23014e9502148d2
Jack <zediious@aol.com>
2025-05-18 21:29:32 -0400

    Don't redirect non-hx requests if NoProcessHxRedirect header present

--- raptorWeb/raptormc/hx_reroute_middleware.py
+++ raptorWeb/raptormc/hx_reroute_middleware.py
@@ -10,6 +10,10 @@ class HxReroute(MiddlewareMixin):
 
     def process_response(self, request: HttpRequest, response: HttpResponse) -> HttpResponse:
 
+        # Skip processing if NoProcessHxRedirect header present
+        if (request.headers.get('Noprocesshxredirect') == "true"):
+            return response
+
         # 404 the request if HX-Request is not true
         if ((request.headers.get('HX-Request') != "true") and ('/api/' in request.path)):
             return HttpResponseRedirect('/404')
